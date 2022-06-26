b9f155d430eb48e764f76be916bccada7d0faa61
dyyyzzz <nuradilah688@gmail.com>
2022-06-26 15:34:07 +0800

    commit

--- admin/stafflogin.php
+++ admin/stafflogin.php
@@ -7,7 +7,7 @@
 "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="login.css">
+	<link rel="stylesheet" href="css/login.css">
 	<title>Staff Login Page</title>
 </head>
 
@@ -30,7 +30,6 @@
 
 			<input class="button" type="submit"
 					name="login" value="Sign In">
-			<a href="login.php">User</a>
 		</div>
 	</form>
 </body>
