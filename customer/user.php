d2c741caaecf29724264059c09dc22537101c25c
IzzatMaswan <103076901+IzzatMaswan@users.noreply.github.com>
2022-06-29 02:17:13 +0800

    compc1

--- customer/user.php
+++ customer/user.php
@@ -1,52 +1,40 @@
 <?php 
 #Script handle form
 
-if (strlen($_POST["name"]) >0)
+if (strlen($_POST["Cust_username"]) >0)
     { 
-    $_POST["name"] = $_POST["name"];
+    $_POST["Cust_username"] = $_POST["Cust_username"];
     }
 else
     {
-    $_POST["name"]= null;
+    $_POST["Cust_username"]= null;
+    echo '<p><b> You Forgot to Enter Your Userame! </b></p>';
+    }
+
+if (strlen($_POST["Cust_name"]) >0)
+    { 
+    $_POST["Cust_name"] = $_POST["Cust_name"];
+    }
+else
+    {
+    $_POST["Cust_name"]= null;
     echo '<p><b> You Forgot to Enter Your Name! </b></p>';
     }
 
 
-if (strlen($_POST["complain"]) >0) 
+if (strlen($_POST["Comp_Description"]) >0) 
     { 
-    $_POST["complain"] = $_POST["complain"]; 
+    $_POST["Comp_Description"] = $_POST["Comp_Description"]; 
     }
 else
     {
-    $_POST["complain"]= null;
-    echo '<p><b> You Forgot to Enter Your complain! </b></p>';
-    }
-if(! (strlen($_POST["email"]) >0)) 
-    {
-    $_POST["email"] = null;
-    echo '<p><b> You Forgot to Enter Your Emails! </b></p>';
+    $_POST["Comp_Description"]= null;
+    echo '<p><b> You Forgot to Enter Your Complain! </b></p>';
     }
 
-
-if (isset($_POST["gender"])) 
-    {
-    if ($_POST["gender"] == 'M') 
-    {
-        $message = '<b><p>Good Day, Sir </b></p>';
-    }
-    if ($_POST["gender"] == 'F') 
-    {
-        $message = '<b><p>Good Day, Madam </b></p>';
-    }
-}else{
-        $_POST["gender"] = null;
-        echo '<p><b> You Forgot to Choose Your Gender! </b></p>';
-    }
-
-
-if ($_POST["name"] && $_POST["complain"] && $_POST["email"] && $_POST["gender"]) 
-    { echo "Thank you," .$_POST["name"]. " for the following complain: <br /> <tt>" .
-    $_POST["complain"]. " </tt> <p> We will reply to you at <i>" . $_POST["email"]. 
+if ($_POST["Cust_username"] && $_POST["Cust_name"] && $_POST["Comp_Description"] ) 
+    { echo "Thank you," .$_POST["Cust_username"]. " for the following Complain: <br /> <tt>" .
+    $_POST["Comp_Description"]. " </tt> <p> We will reply to you at <i>" . 
     "</i></p>"; 
     echo $message;
     }
