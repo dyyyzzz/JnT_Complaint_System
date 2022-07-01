ac10f064e9ec028d2b1ae0ae24f7407cd6716f33
dyyyzzz <nuradilah688@gmail.com>
2022-07-02 02:58:14 +0800

    commit

--- customer/logout.php
+++ customer/logout.php
@@ -1,7 +1,15 @@
-<?php 
+<?php
+// Initialize the session
 session_start();
-
-session_unset();
+ 
+// Unset all of the session variables
+$_SESSION = array();
+ 
+// Destroy the session.
 session_destroy();
 
-header("Location: index.php");
+ 
+// Redirect to login page
+header("location: login.php");
+exit;
+?>
