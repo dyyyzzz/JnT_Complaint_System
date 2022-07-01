df1e761cea229574b0c346640a3b46b4e978841d
dyyyzzz <nuradilah688@gmail.com>
2022-07-01 10:53:06 +0800

    commit

--- customer/register.php
+++ customer/register.php
@@ -79,8 +79,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to login page
                 header("location: login.php");
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
             }
 
             // Close statement
