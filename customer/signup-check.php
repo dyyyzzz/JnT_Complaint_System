3e5d2331ffdefccd7c13416f6203361e836d5855
dyyyzzz <nuradilah688@gmail.com>
2022-07-01 14:38:43 +0800

    commit

--- customer/signup-check.php
+++ customer/signup-check.php
@@ -48,14 +48,14 @@ if (isset($_POST['Cust_Username']) && isset($_POST['Cust_Password'])
 		// hashing the password
         $pass = md5($pass);
 
-	    $sql = "SELECT * FROM users WHERE user_name='$uname' ";
+	    $sql = "SELECT * FROM customer WHERE Cust_Username='$uname' ";
 		$result = mysqli_query($conn, $sql);
 
 		if (mysqli_num_rows($result) > 0) {
 			header("Location: signup.php?error=The username is taken try another&$user_data");
 	        exit();
 		}else {
-           $sql2 = "INSERT INTO users(user_name, password, name) VALUES('$uname', '$pass', '$name')";
+           $sql2 = "INSERT INTO users(Cust_Username, Cust_Password, Cust_Name) VALUES('$uname', '$pass', '$name')";
            $result2 = mysqli_query($conn, $sql2);
            if ($result2) {
            	 header("Location: signup.php?success=Your account has been created successfully");
